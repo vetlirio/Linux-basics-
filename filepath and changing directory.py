24f62e3dfc17988cc73e102cf1111633bb727667
vetlirio <vetle.pt@gmail.com>
2025-05-08 00:15:42 +0200

    Add files via upload

---
+++ filepath and changing directory.py
@@ -0,0 +1,44 @@
+pwd = print current directory (filepath)
+cd .. = go back to previous directory on the path
+cd = change directory "cd worldbanc"
+cd ~ = home directory
+
+codemaster@Money:~/worldbanc$ ls
+README.md  private  public
+codemaster@Money:~/worldbanc$ pwd
+/home/codemaster/worldbanc
+codemaster@Money:~/worldbanc$
+codemaster@Money:~/worldbanc$
+codemaster@Money:~/worldbanc$
+codemaster@Money:~/worldbanc$
+codemaster@Money:~/worldbanc$ cd ..
+codemaster@Money:~$ pwd
+/home/codemaster
+codemaster@Money:~$ ls
+worldbanc
+codemaster@Money:~$ cd ..
+codemaster@Money:/home$ ls
+codemaster
+codemaster@Money:/home$ pwd
+/home
+codemaster@Money:/home$ cd ..
+codemaster@Money:/$ ls
+bin                dev   init               lib64       mnt   root  sbin.usr-is-merged  sys  var
+bin.usr-is-merged  etc   lib                lost+found  opt   run   snap                tmp
+boot               home  lib.usr-is-merged  media       proc  sbin  srv                 usr
+codemaster@Money:/$ pwd
+/
+codemaster@Money:/$
+codemaster@Money:/$
+codemaster@Money:/$ cd /home
+codemaster@Money:/home$ cd codemaster
+codemaster@Money:~$ cd worldbanc
+codemaster@Money:~/worldbanc$ pwd
+/home/codemaster/worldbanc
+codemaster@Money:~/worldbanc$ ls
+README.md  private  public
+codemaster@Money:~/worldbanc$
+codemaster@Money:~/worldbanc$ cd ~
+codemaster@Money:~$ pwd
+/home/codemaster
+codemaster@Money:~$
